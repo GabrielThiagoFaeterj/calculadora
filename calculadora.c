c209851e4ca11fc48823fc69e19f075ef3ae3ab2
GBTHFaeterj <gabiel.25104708360066@faeterj-rio.edu.br>
2025-03-21 11:45:01 -0300

    calc v2

--- calculadora.c
+++ calculadora.c
@@ -3,11 +3,11 @@
 
 void main(){
 
-	float a, b, soma, subtracao, divisao;
+	float a, b, soma, subtracao, divisao, multiplicacao;
 	int opcao;
+
 	printf("Digite o 1º numero: ");
 	scanf("%f", &a);
-    //oi
 
 	printf("Digite o 2º numero: ");
 	scanf("%f", &b);
@@ -15,8 +15,9 @@ void main(){
 	soma = a+b;
 	subtracao = a-b;
 	divisao = a/b;
-	
-	printf("Escolha a operacao desejada:\n 1 - Soma\n 2 - Subtracao\n 3 - Divisao\n\n");
+	multiplicacao = a*b;
+
+	printf("Escolha a operacao desejada:\n 1 - Soma\n 2 - Subtracao\n 3 - Divisao\n 4 - Multiplicacao\n 5 - Divisao com resto\n");
 	scanf("%d", &opcao);
 
 
@@ -25,11 +26,17 @@ void main(){
         case 1: soma;
             printf("\nO resultado da sua operacao e: %.2f\n\n", soma);
             break;
+            
         case 2: subtracao;
             printf("\nO resultado da sua operacao e: %.2f\n\n", subtracao);
             break;
+
         case 3: divisao;
             printf("\nO resultado da sua operacao e: %.2f\n\n", divisao);
+            break;
+
+        case 4: multiplicacao;
+            printf("\nO resultado da sua operacao e: %.2f\n\n", multiplicacao);
             break;
     }
 }
